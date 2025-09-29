56181ffee96f47f73f6b9b9231570e534683632c
Marcelo Napoleão <73085259+marcelonap@users.noreply.github.com>
2025-09-29 10:22:58 -0600

    uploading week 5 demos

---
+++ week5/le5Demo3/js/jsdemo.js
@@ -0,0 +1,9 @@
+console.log("Hi. It's nice to know that the page has been loaded");
+       function testFunction(){
+         alert("Hello! How are you?");
+         document.body.style.background="cyan";
+       }
+
+       function consoleLog(){d
+          console.log("I was called frm the console \n style: " + document.body.style.background)
+       }
