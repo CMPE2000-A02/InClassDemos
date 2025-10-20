04a670a65b2d1e2aa55dd4cac1188a03df108199
Marcelo Napoleão <73085259+marcelonap@users.noreply.github.com>
2025-10-20 10:53:31 -0600

    Uploading demo 6 (not done)

--- week5/le5Demo3/js/jsdemo.js
+++ week5/le5Demo3/js/jsdemo.js
@@ -3,7 +3,7 @@ console.log("Hi. It's nice to know that the page has been loaded");
          alert("Hello! How are you?");
          document.body.style.background="cyan";
        }
-
+      // This function is triggered in the console and is an example of basic JS
        function consoleLog(){d
           console.log("I was called frm the console \n style: " + document.body.style.background)
        }
---
+++ week6/week6/le6Demo4/js/le6Demo4.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+/*
+function buildSection() {
+    let str = "";
+    str = "<section id='newSection'>"
+    str = str + "<p> Hello Friends - How are you? <\p>";
+    str = str + "<p>This is Exiting<\p>";
+    str = str + "</section>";
+    let elem = document.querySelector("#placeHolder2");
+    elem.innerHTML = str;
+}
+*/
+
+/*
+function buildBigSection() {
+    let str = "";
+    let counter = 0;
+    str = "<section>"
+
+    while (counter < 10) {
+        str = str + "<p> paragraph " + (counter + 1) + "</p>";
+        counter++;
+    }
+
+    str = str + "</section>";
+    let elem = document.getElementById("placeHolder3");
+    elem.innerHTML = str;
+}
+
+*/
